749132a3c9e81d98cee8e6eb8f35593d53b986f0
ruben <24549041+RDieleman@users.noreply.github.com>
2022-11-06 19:23:34 +0100

    Updates

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,12 +1,14 @@
-import { Request, Response, NextFunction } from "express";
-import { CustomError } from '../errors/custom-error';
+import {NextFunction, Request, Response} from "express";
+import {CustomError} from '../errors/custom-error';
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof CustomError) {
-    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+    return res.status(err.statusCode).send({errors: err.serializeErrors()});
   }
 
+  console.error(err);
+
   res.status(400).send({
-    errors: [{ message: err.message }]
+    errors: [{message: err.message}]
   });
 };
